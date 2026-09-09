5494f245ed894ee6dc9084970a6f32ed7ba9831b
Schwapp <150485443+Schwapp@users.noreply.github.com>
2026-09-09 20:16:01 +0200

    Fix console log for empty cart message

--- instagram.js
+++ instagram.js
@@ -3,5 +3,5 @@ let count = "0";
 if (count!=0) {
     console.log("У вас есть товары");
 } else {
-    console.log("Корзина пуста"); // Должно сработать это при "0"
+    console.log("Корзина пуста"); 
 }
