3d3e8bef047067813303199c8a4396325f519f48
Sergey Vinichenko <kirimachacho@gmail.com>
2026-09-13 11:47:29 +0300

    подправил структуру поста

--- models/model_post_user.js
+++ models/model_post_user.js
@@ -1,35 +1,21 @@
 const {DataTypes} = require('sequelize');
-const sequelize = require('../config/database');
+const sequelize = require('D:/JavaScript/my-network/db.js');
 
 const PostUser = sequelize.define('PostUser', {
-  id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true
-  },
+  
   userId: {
     type: DataTypes.INTEGER,
     allowNull: false,
-    references: {
-      model: 'Users',
-      key: 'id'
-    }
+    
   },
-  postId: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    references: {
-      model: 'Posts',
-      key: 'id'
-    }
-  }
+  
 }, {
   tableName: 'post_users',
   timestamps: true
 },
 { text_of_post:  {
     type: DataTypes.STRING,
-    allowNull: true
+    
   },
   author: {
     type: DataTypes.STRING,
