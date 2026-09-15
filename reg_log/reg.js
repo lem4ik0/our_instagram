35f840c8241f080cbf7fdcaa8bcf3c46503f760a
Sergey Vinichenko <kirimachacho@gmail.com>
2026-09-15 19:47:36 +0300

    добавил логин пользователя и подправил регистрацию

--- reg_log/reg.js
+++ reg_log/reg.js
@@ -6,6 +6,18 @@ const jwt = require('jsonwebtoken');
 const User = require('../models/model_user.js');
 routerSignup.post('/signup', async (req, res) => {
   const { username, email, password } = req.body;
+  const existingUser = await User.findOne({ where: { email: email } });
+  if (existingUser) {
+    return res.status(400).json({
+      message: 'User email already exists'
+    })}
+    const existingUsername = await User.findOne({ where: { username: username } });
+  if (existingUsername) {
+    return res.status(400).json({
+      message: 'Username already exists'
+    })
+    
+  }
   try{
   const hash = await bcrypt.hash(req.body.password, 10);
   
@@ -13,19 +25,21 @@ routerSignup.post('/signup', async (req, res) => {
         username: req.body.username,
         email: req.body.email,
         password: hash
-      });
+      })
      const result = await user.save();
+     const safeUser = result.toJSON();
+     safeUser.password = undefined;
           return res.status(201).json({
             message: 'User created successfully',
-            user: result
-          });
+            user: safeUser
+          })
         
         }catch (err) {
           console.error(err);  
         return res.status(500).json({
           message: 'Error creating user',
           error: err.message
-        });
+        })
       }
           });
      
